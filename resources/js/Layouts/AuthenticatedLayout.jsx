fab3f2254339167f11a96124295747545db0cc35
Wallace Ferreira <wallace.sf87@gmail.com>
2023-01-22 15:44:52 -0300

    :art: renames link

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -93,7 +93,7 @@ export default function Authenticated({ auth, header, children }) {
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('bookstore.index')} active={route().current('bookstore.index')}>
-                            Dashboard
+                            Bookstore
                         </ResponsiveNavLink>
                     </div>
 
